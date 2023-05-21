7e06e46875f86581684b12bbcf6744e310e4a056
drew08 <anrincongo@unal.edu.co>
2023-05-21 11:20:27 -0500

    sidebar bug fix
    sidebar bug fix

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,35 +1,35 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { untilDestroyed } from '@ngneat/until-destroy';
-import { delay, filter } from 'rxjs/operators';
+import { Router, RouterModule } from '@angular/router';
 import { SideNavService } from 'src/app/services/side-nav.service';
 
-  
+
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, RouterModule, MatDividerModule , MatIconModule],
+  imports: [CommonModule, MatSidenavModule, RouterModule, MatDividerModule, MatIconModule],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent {
 
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
 
-  constructor(private observer: BreakpointObserver, private router: Router, private sideNavService: SideNavService) {}
-  ngOnInit(): void {
-    this.sideNavService.sideNavToggleSubject.subscribe(()=> {
-      this.sidenav.toggle();
-    });
-  }
+
+  constructor(private observer: BreakpointObserver, private router: Router, private sideNavService: SideNavService) { }
+
 
   ngAfterViewInit() {
+
+    this.sideNavService.sideNavToggleSubject.subscribe(() => {
+      this.sidenav.toggle();
+    });
+
     this.observer.observe(['(max-width: 800px)'])
       .subscribe((res) => {
         if (res.matches) {
@@ -41,5 +41,8 @@ export class SidenavComponent implements OnInit {
         }
       });
 
+
   }
+
+
 }
