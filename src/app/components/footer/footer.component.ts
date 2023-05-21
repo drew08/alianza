0121cfa23d4fddc8a2dc44ab8ac7521e81f82892
drew08 <anrincongo@unal.edu.co>
2023-05-20 23:56:57 -0500

    add icons to footer
    add icons to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
